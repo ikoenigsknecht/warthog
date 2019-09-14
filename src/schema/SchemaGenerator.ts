ba561c9239ded0d5aab713b90aacce5f143b27ee
Dan Caddigan <goldcaddy77@gmail.com>
2019-09-14 16:28:56 -0400

    feat(code-generator): generate from metadata instead of DB connection

--- src/schema/SchemaGenerator.ts
+++ src/schema/SchemaGenerator.ts
@@ -40,9 +40,8 @@ export class SchemaGenerator {
       ${generateEnumMapImports().join('')}
     `;
 
-    console.log('metadata: ', metadata);
-
-    console.log(metadata.getModels());
+    // console.log('metadata: ', metadata);
+    logger.logObject(metadata.getModels());
 
     Object.keys(metadata.getModels()).forEach((modelName: string) => {
       const model: ModelMetadata = metadata.getModel(modelName);
